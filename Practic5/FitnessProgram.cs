b745d788d1a3f0b60a22219f9e6ae883c2fd551d
SergeyKul1kov <fabygoodi@gmail.com>
2024-12-17 22:26:02 +0700

    Add files via upload

---
+++ Practic5/FitnessProgram.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practic5
+{
+    class FitnessProgram
+    {
+
+        private int CaloriesBurned { get; set; }
+        private int TotalExcesises { get; set; }
+        private string[] motivationMessageArray = 
+        {
+            "Вы можете больше!",
+            "Двигайтесь к своей цели!",
+            "Не останавливайтесь, вы на верном пути!"
+        };
+        private Random randomKey = new Random();
+
+        public string Name { get; }
+        public List<string> ExercisePlan { get; set; }
+        public int DailyCaloriesGoal { get; set; }
+
+        public FitnessProgram(string name, string[] exercises)
+        {
+            Name = name;
+            ExercisePlan = new List<string>(exercises);
+        }
+        
+        public string ShowFithessLevelInformation()
+        {
+            string level;
+            if (CaloriesBurned > 2000) level = "Спортсмен";
+            else if (CaloriesBurned > 1000) level = "Активный";
+            else level = "Новичок";
+
+            string warningMessage = Warning() ? "Цель достигнута!" : "Цель достигнута!";
+            return $"Количество тренировок: {TotalExcesises} \nУровень тренировок: {level}\n{warningMessage}";
+        }
+
+        public bool Warning()
+        {
+            return CaloriesBurned < DailyCaloriesGoal;
+        }
+
+        public void CompleteExercise(int calories)
+        {
+            if (ExercisePlan.Count > 0)
+            {
+                CaloriesBurned += calories;
+                TotalExcesises++;
+                ExercisePlan.RemoveAt(0);
+            }
+            else
+            {
+                Console.WriteLine("Нет доступных тренировок в плане!");
+            }
+        }
+
+        public string Motivate()
+        {
+            int index = randomKey.Next(motivationMessageArray.Length);
+            return motivationMessageArray[index];
+        }
+
+        public void SetExercisePlan(string[] exercises)
+        {
+            ExercisePlan = new List<string>(exercises);
+        }
+
+        public void AddExercises(string exercises)
+        {
+            ExercisePlan.Add(exercises);
+        }
+
+        public void RemoveExercises(string exercises)
+        {
+            ExercisePlan.Remove(exercises);
+        }
+    }
+}
---
+++ Practic5/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practic5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите план тренировок (через запятую): ");
+
+            string[] exercises = Console.ReadLine().Split(',');
+
+            FitnessProgram fitnessProgram = new FitnessProgram(name, exercises);
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Выполнить упражнение");
+                Console.WriteLine("2. Вывести текущую информацию об уровне физической подготовке");
+                Console.WriteLine("3. Установить целевую норму калорий");
+                Console.WriteLine("4. Задать новый план тренировок");
+                Console.WriteLine("5. Изменить план тренировок");
+                Console.WriteLine("6. Вывести мотивационное сообщение");
+                Console.WriteLine("0. Выход");
+                Console.Write("Выберите пункт меню: ");
+                int choise = Convert.ToInt32(Console.ReadLine());
+
+                switch (choise)
+                {
+                    case 1:
+                        Console.Write("Введите количество сожженых калорий: ");
+                        int calories = Convert.ToInt32(Console.ReadLine());
+                        fitnessProgram.CompleteExercise(calories);
+                        break;
+                    case 2:
+                        Console.WriteLine(fitnessProgram.ShowFithessLevelInformation());
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.Write("Введите целевую норму калорий: ");
+                        fitnessProgram.DailyCaloriesGoal = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 4:
+                        Console.Write("Введите новый план тренировок (через запятую): ");
+                        exercises = Console.ReadLine().Split(',');
+                        fitnessProgram.SetExercisePlan(exercises);
+                        break;
+                    case 5:
+                        Console.WriteLine("1. Удалить тренировку");
+                        Console.WriteLine("2. Добавить тренировку");
+
+                        int modifyChoice = Convert.ToInt32(Console.ReadLine());
+
+                        switch (modifyChoice)
+                        {
+                            case 1:
+                                Console.Write("Введите название тренировки для удаления: ");
+                                string exercisesToRemove = Console.ReadLine();
+                                fitnessProgram.RemoveExercises(exercisesToRemove);
+                                break;
+                            case 2:
+                                Console.Write("Введите название тренировки для добавления: ");
+                                string exercisesToAdd = Console.ReadLine();
+                                fitnessProgram.AddExercises(exercisesToAdd);
+                                break;
+                        }
+                        break;
+                    case 6:
+                        Console.WriteLine(fitnessProgram.Motivate());
+
+                        Console.ReadLine();
+                        break;
+                    case 0:
+                        return;
+                    default:
+                        Console.WriteLine("Неверный ввод. Попробуйте сноваю");
+                        break;
+
+                }
+            }
+        }
+    }
+}
